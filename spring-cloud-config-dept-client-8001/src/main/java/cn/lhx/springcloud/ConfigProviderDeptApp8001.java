9683cb5843435c7f3b94a65c9fbe6bcbdfb3ca7d
leee549 <1193106371@qq.com>
2020-06-28 18:54:27 +0800

    config done

---
+++ spring-cloud-config-dept-client-8001/src/main/java/cn/lhx/springcloud/ConfigProviderDeptApp8001.java
@@ -0,0 +1,22 @@
+package cn.lhx.springcloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lee549
+ * @date 2020/6/19 17:55
+ */
+@SpringBootApplication
+@EnableEurekaClient    //本服务启动后会自动注册进Eureka服务中
+@EnableDiscoveryClient  //服务发现
+@MapperScan("cn.lhx.springcloud.dao")
+public class ConfigProviderDeptApp8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigProviderDeptApp8001.class,args);
+    }
+
+}
